95a987e4c8ca90bfa7002b986b986e89529e26e3
luigifusco <luigifusco98@gmail.com>
2020-04-05 15:30:34 +0200

    add generics for function and size

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,12 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-float windowFunction(int bin, int value) {
+inline float histogram(int bin, int value) {
     return bin == value? 1 : 0;
 }
 
-float parzenEstimator(unsigned char* If, unsigned char* Im, int i_bin, int k_bin, int size) {
+template<float (*windowFunction)(int, int), int size>
+inline float parzenEstimator(unsigned char* If, unsigned char* Im, int i_bin, int k_bin) {
     float sum = 0;
 
     for(int i = 0; i < size; ++i) {
@@ -20,7 +21,8 @@ float parzenEstimator(unsigned char* If, unsigned char* Im, int i_bin, int k_bin
     return sum;
 }
 
-float mutualInformation(unsigned char* If, unsigned char* Im, int size) {
+template<float (*windowFunction)(int, int), int size>
+float mutualInformation(unsigned char* If, unsigned char* Im) {
     float sum = 0;
 
     float normalizationFactor = 0;
@@ -30,7 +32,7 @@ float mutualInformation(unsigned char* If, unsigned char* Im, int size) {
 
     for(int i = 0; i < 256; ++i) {
         for(int k = 0; k < 256; ++k) {
-            estimators[i][k] = parzenEstimator(If, Im, i, k, size);
+            estimators[i][k] = parzenEstimator<windowFunction, size>(If, Im, i, k);
             normalizationFactor += estimators[i][k];
         }
     }
@@ -45,25 +47,24 @@ float mutualInformation(unsigned char* If, unsigned char* Im, int size) {
 
     for(int i = 0; i < 256; ++i) {
         float partProb_i = partial_i_estimators[i];
-            for(int k = 0; k < 256; ++k) {
-                float partProb_k = partial_k_estimators[k];
-                if(partProb_k != 0) {
-                    float prob = estimators[i][k];
-                        if(prob != 0) {
-                            sum += prob * log2(prob / (partProb_i * partProb_k));
-                        }
-                }
+        for(int k = 0; k < 256; ++k) {
+            float partProb_k = partial_k_estimators[k];
+            if(partProb_k != 0) {
+                float prob_ik = estimators[i][k];
+                    if(prob_ik != 0) {
+                        sum += prob_ik * log2(prob_ik / (partProb_i * partProb_k));
+                    }
             }
         }
     }
 
-    return -sum;
+    return sum;
 }
 
 int main(int argc, char **argv) {
     cv::Mat img = imread("rock_landscape.jpg", cv::IMREAD_GRAYSCALE);
 
-    std::cout << mutualInformation(img.data, img.data, 100000) << std::endl;
+    std::cout << mutualInformation<histogram, 1000>(img.data, img.data) << std::endl;
 
     return 0;
 }
